4fe49cc2d76b60f23f11f00abd90686278db4470
Alex Kim <alex.jg.kim@gmail.com>
2025-03-28 16:24:05 -0400

    removing experience for now

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './components/Hero/Hero';
 import { About } from './components/About/About';
-import { Experience } from './components/Experience/Experience';
+// import { Experience } from './components/Experience/Experience';
 function App() {
 
   return (
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Experience />
+
     </div>
   )
 
